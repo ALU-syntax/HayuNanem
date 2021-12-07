c0e38e24cef387b92659e17a1376cee7a024e084
woodiechek <68280149+ALU-syntax@users.noreply.github.com>
2021-12-07 15:56:45 +0700

    first commit

---
+++ app/src/main/java/com/Kelompok1/android/hayunanem/MainActivity.java
@@ -0,0 +1,14 @@
+package com.Kelompok1.android.hayunanem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
