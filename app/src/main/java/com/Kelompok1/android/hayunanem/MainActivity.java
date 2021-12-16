f76a713aec47c0ea3ba89c0edf23017cab4f7a82
woodiechek <68280149+ALU-syntax@users.noreply.github.com>
2021-12-16 22:23:21 +0700

    Join Fragment
    MainActivity Update
    All Fragment Update
    Create Navigation Bottom

--- app/src/main/java/com/Kelompok1/android/hayunanem/MainActivity.java
+++ app/src/main/java/com/Kelompok1/android/hayunanem/MainActivity.java
@@ -1,14 +1,55 @@
 package com.Kelompok1.android.hayunanem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.Kelompok1.android.hayunanem.Fragment.HomeFragment;
+import com.Kelompok1.android.hayunanem.Fragment.MarketListFragment;
+import com.Kelompok1.android.hayunanem.Fragment.ReminderFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        navigationView = findViewById(R.id.bottom_navigation);
+        getSupportFragmentManager().beginTransaction().replace(R.id.body_container, new HomeFragment()).commit();
+        navigationView.setSelectedItemId(R.id.nav_home);
+
+        navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = null;
+                switch (item.getItemId()){
+
+                    case R.id.nav_home:
+                        fragment = new HomeFragment();
+                        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                        break;
+
+                    case R.id.nav_reminder:
+                        fragment = new ReminderFragment();
+                        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_left);
+                        break;
+
+                    case R.id.nav_market:
+                        fragment = new MarketListFragment();
+                        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_left);
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.body_container, fragment).commit();
+
+                return true;
+            }
+        });
+
     }
 }
